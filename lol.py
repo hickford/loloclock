28c1fcfabad4be484381f4798c2da32282fe38d0
Matt Hickford <matt.hickford@gmail.com>
2011-04-08 23:57:21 +0100

    loop

--- lol.py
+++ lol.py
@@ -5,45 +5,49 @@ import urlparse
 import os.path
 import bitly
 home = "http://twitter.com/lol_o_clock"
+print home
 
 links_url = "http://www.b3ta.com/links/popular/"
 homemade_url = "http://www.b3ta.com/links/popular/?i=1" # b3tan's own
-url = homemade_url
+#url = homemade_url
 
-page = urllib2.urlopen(url)
-soup = BeautifulSoup(page)
-
-posts = soup.findAll('div',attrs={'class':['post1','post2']})
-
-post=posts[0]
-A = post.findAll('a')
-
-burl = urlparse.urljoin(url,A[1]['href'])
-sburl = urlparse.urljoin(url,A[-1]['href'])
-link = A[2]['href']
-title = A[2].string
-
-if post.find(attrs={'class':'imadethis'}):
-  link = sburl
-
-import bitly
-bcreds= open(os.path.expanduser('~/.bitly_creds')).readlines()
-blogin = bcreds[0].strip()
-bapikey = bcreds[1].strip()
-bapi = bitly.Api(login=blogin,apikey=bapikey)
-slink = bapi.shorten(link)
-
-tweet = "%s %s" % (title,link)
-if len(tweet) > 140:
-	tweet = "%s %s" % (title,link)
-
-print home
-print tweet
-print len(tweet)
 from twitter import Twitter, NoAuth, OAuth, read_token_file
 from twitter.cmdline import CONSUMER_KEY, CONSUMER_SECRET
-import os.path
-
 oauth = OAuth(*read_token_file(os.path.expanduser('~/.twitter_oauth')) + (CONSUMER_KEY, CONSUMER_SECRET))
 twitter = Twitter(domain='api.twitter.com',auth=oauth, api_version='1')
-twitter.statuses.update(status=tweet)
+
+for url in [links_url,homemade_url]:
+
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+    posts = soup.findAll('div',attrs={'class':['post1','post2']})
+
+    # post=posts[0]
+
+    for post in posts:
+        A = post.findAll('a')
+
+        burl = urlparse.urljoin(url,A[1]['href'])
+        sburl = urlparse.urljoin(url,A[-1]['href'])
+        link = A[2]['href']
+        title = A[2].string
+
+        if post.find(attrs={'class':'imadethis'}):
+            link = sburl
+
+        bcreds= open(os.path.expanduser('~/.bitly_creds')).readlines()
+        blogin = bcreds[0].strip()
+        bapikey = bcreds[1].strip()
+        bapi = bitly.Api(login=blogin,apikey=bapikey)
+        slink = bapi.shorten(link)
+
+        tweet = "%s %s" % (title,link)
+        if len(tweet) > 140:
+            tweet = "%s %s" % (title,link)
+
+        print tweet
+
+        try:
+            twitter.statuses.update(status=tweet)
+        except twitter.api.TwitterHTTPError, E:
+            pass
