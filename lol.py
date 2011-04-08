7e41fc3c2e3cd75ddef31a46a5fcc000fce8334d
Matt Hickford <matt.hickford@gmail.com>
2011-04-08 23:36:27 +0100

    tweet. shorten url if need be

--- lol.py
+++ lol.py
@@ -1,11 +1,13 @@
-#!python
+#!/usr/bin/python
 from BeautifulSoup import BeautifulSoup
 import urllib2
 import urlparse
 
+home = "http://twitter.com/lol_o_clock"
+
 links_url = "http://www.b3ta.com/links/popular/"
 homemade_url = "http://www.b3ta.com/links/popular/?i=1" # b3tan's own
-url = links_url
+url = homemade_url
 
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page)
@@ -22,6 +24,24 @@ title = A[2].string
 if post.find(attrs={'class':'imadethis'}):
   link = sburl
 
-tweet = "%s %s" % (title,link)
-print tweet
+import bitly
+bcreds= open('bitly_creds').readlines()
+blogin = bcreds[0].strip()
+bapikey = bcreds[1].strip()
+bapi = bitly.Api(login=blogin,apikey=bapikey)
+slink = bapi.shorten(link)
 
+tweet = "%s %s" % (title,link)
+if len(tweet) > 140:
+	tweet = "%s %s" % (title,link)
+
+print home
+print tweet
+print len(tweet)
+from twitter import Twitter, NoAuth, OAuth, read_token_file
+from twitter.cmdline import CONSUMER_KEY, CONSUMER_SECRET
+import os.path
+
+oauth = OAuth(*read_token_file('twitter_oauth') + (CONSUMER_KEY, CONSUMER_SECRET))
+twitter = Twitter(domain='api.twitter.com',auth=oauth, api_version='1')
+twitter.statuses.update(status=tweet)
