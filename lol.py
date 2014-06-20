1800fd8929c45aa05d3971e35451fc691cad1754
Matt Hickford <matt.hickford@gmail.com>
2014-06-20 01:05:16 +0100

    python3 compatibility but shortening broken

--- lol.py
+++ lol.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import json
-import urllib2
-import urlparse
+import requests
+from six.moves.urllib_parse import urljoin
 import os.path
-import bitly
 home = "http://twitter.com/lol_o_clock"
-print home
+print(home)
 
 links_url = "http://www.b3ta.com/links/popular/"
 homemade_url = "http://www.b3ta.com/links/popular/?i=1" # b3tan's own
-#url = homemade_url
 
 import twitter
 import twitter.cmdline
 
 oauth = twitter.OAuth(*twitter.read_token_file(os.path.expanduser('~/.twitter_oauth')) + (twitter.cmdline.CONSUMER_KEY, twitter.cmdline.CONSUMER_SECRET))
-bird = twitter.Twitter(domain='api.twitter.com',auth=oauth, api_version='1')
+bird = twitter.Twitter(domain='api.twitter.com',auth=oauth, api_version='1.1')
 
 for url in [homemade_url,links_url]:
 
-    page = urllib2.urlopen(url)
+    response = requests.get(url)
+    response.raise_for_status()
+    page = response.content
     soup = BeautifulSoup(page)
     posts = soup.findAll('div',attrs={'class':['post1','post2']})
-
-    # post=posts[0]
 
     for post in posts:
         A = post.findAll('a')
 
-        burl = urlparse.urljoin(url,A[1]['href'])
-        sburl = urlparse.urljoin(url,A[-1]['href'])
+        burl = urljoin(url,A[1]['href'])
+        sburl = urljoin(url,A[-1]['href'])
         link = A[2]['href']
         title = A[2].string
 
@@ -39,17 +37,22 @@ for url in [homemade_url,links_url]:
             if url == links_url:
                 continue            
              
-        bcreds= open(os.path.expanduser('~/.bitly_creds')).readlines()
-        blogin = bcreds[0].strip()
-        bapikey = bcreds[1].strip()
-        bapi = bitly.Api(login=blogin,apikey=bapikey)
-        slink = bapi.shorten(link)
-
         tweet = "%s %s" % (title,link)
         if len(tweet) > 140:
-            tweet = "%s %s" % (title,link)
+            continue
 
-        print tweet
+            # broken
+
+            import bitly
+
+            bcreds= open(os.path.expanduser('~/.bitly_creds')).readlines()
+            blogin = bcreds[0].strip()
+            bapikey = bcreds[1].strip()
+            bapi = bitly.Api(login=blogin,apikey=bapikey)
+            slink = bapi.shorten(link)
+            tweet = "%s %s" % (title,slink)
+
+        print(tweet)
 
         try:
             bird.statuses.update(status=tweet)
@@ -57,10 +60,10 @@ for url in [homemade_url,links_url]:
         except twitter.api.TwitterHTTPError as E:
            # print E
            reason = json.loads(E.e.fp.read())['error']
-           print reason
+           print(reason)
            if "duplicate" in reason:
                 break
            else:
                 E.e.fp.write(reason)
-                raise twitter.api.TwitterHTTPError, E
+                raise twitter.api.TwitterHTTPError(E)
      
